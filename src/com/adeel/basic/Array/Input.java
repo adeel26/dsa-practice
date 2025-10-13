20207ea9f36350e2e797a4b9f0ee114ef1233c4b
Adeel Iqbal <iadeel26@gmail.com>
2025-10-13 12:25:44 -0700

    Array: Input file added

---
+++ src/com/adeel/basic/Array/Input.java
@@ -0,0 +1,39 @@
+package com.adeel.basic.Array;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] arr = new int[5];
+        arr[0] = 23;
+        arr[1] = 45;
+        arr[2] = 233;
+        arr[3] = 543;
+        arr[4] = 3;
+        // [23, 45, 233, 543, 3]
+
+        System.out.println(arr[3]);
+
+        // input using for loop
+        int[] customArray = new int[5];
+        for (int i = 0; i < customArray.length; i++){
+            System.out.print("Enter array date for index " + i + " = ");
+            customArray[i] = input.nextInt();
+        }
+        System.out.println(customArray[3]);
+
+        // Print using for loop
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // Print using enhanced loop
+        for( int i : arr) {
+            System.out.println(i + "");
+        }
+
+    }
+}
