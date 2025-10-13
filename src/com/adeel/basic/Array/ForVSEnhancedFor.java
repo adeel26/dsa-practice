4e0ea63ae4500824e9ff2962b1d7e0bee0c5d2b9
Adeel Iqbal <iadeel26@gmail.com>
2025-10-13 12:54:22 -0700

    Array: For VS Enhanced for loop file updated

--- src/com/adeel/basic/Array/ForVSEnhancedFor.java
+++ src/com/adeel/basic/Array/ForVSEnhancedFor.java
@@ -1,4 +1,23 @@
 package com.adeel.basic.Array;
 
 public class ForVSEnhancedFor {
+    public static void main(String[] args) {
+        // Normal For loop
+        System.out.println("Normal for loop");
+        int[] arr = {10, 20, 30};
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(i);      // prints index (0, 1, 2)
+        }
+        System.out.println();
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]); // prints value (10, 20, 30)
+        }
+
+        System.out.println();
+        // Enhanced for loop (for-each)
+        System.out.println("Enhanced for loop");
+        for (int i : arr){
+            System.out.print(i); // prints value directly (10, 20, 30)
+        }
+    }
 }
