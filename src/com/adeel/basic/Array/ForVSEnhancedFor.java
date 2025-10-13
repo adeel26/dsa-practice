e5317cc6935b287536014898cd6926202eec636b
Adeel Iqbal <iadeel26@gmail.com>
2025-10-13 13:01:07 -0700

    Array: For VS Enhanced for loop file updated

--- src/com/adeel/basic/Array/ForVSEnhancedFor.java
+++ src/com/adeel/basic/Array/ForVSEnhancedFor.java
@@ -7,6 +7,7 @@ public class ForVSEnhancedFor {
         int[] arr = {10, 20, 30};
         for (int i = 0; i < arr.length; i++){
             System.out.print(i);      // prints index (0, 1, 2)
+            // Here i is the index variable, not the array element itself.
         }
         System.out.println();
         for (int i = 0; i < arr.length; i++){
@@ -15,9 +16,21 @@ public class ForVSEnhancedFor {
 
         System.out.println();
         // Enhanced for loop (for-each)
+        // Here num represents the value stored at each index — not the index.
         System.out.println("Enhanced for loop");
         for (int i : arr){
             System.out.print(i); // prints value directly (10, 20, 30)
         }
+
+        System.out.println();
+        // If you want both index and value, you can do:
+
+        int index = 0;
+        for (int i : arr){
+            System.out.println("Index " + index + " = " + i);
+            index++;
+        }
+
     }
 }
+
