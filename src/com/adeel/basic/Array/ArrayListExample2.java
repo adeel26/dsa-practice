6b02dc528d7dc3964fb45a3ab62f89fd1bc7518c
Adeel Iqbal <iadeel26@gmail.com>
2025-10-20 23:54:18 -0700

    Array: ArrayList example 2

---
+++ src/com/adeel/basic/Array/ArrayListExample2.java
@@ -0,0 +1,20 @@
+package com.adeel.basic.Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        // Taking input from user
+        for (int i=0; i<5; i++){
+            list.add(input.nextInt());
+        }
+
+        System.out.println(list);
+
+
+    }
+}
