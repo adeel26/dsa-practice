1568c1bd832fa3f6dbd3bb9efa9b1e51fc8f241f
Adeel Iqbal <iadeel26@gmail.com>
2025-10-22 03:00:40 -0700

    Array: Question Max item updated

--- src/com/adeel/basic/Array/Q2_MaxItem.java
+++ src/com/adeel/basic/Array/Q2_MaxItem.java
@@ -12,8 +12,14 @@ public class Q2_MaxItem {
 
     // Imagine array is not empty
     static int Max(int[] array){
+        // edge cases
+        if (array == null || array.length == 0) {
+            return 0;  // or return -1
+        }
+
+        // logic
         int max = array[0];
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) { // starting from 1 is just a small optimization
             if(array[i] > max){
                 max = array[i];
             }
@@ -23,6 +29,16 @@ public class Q2_MaxItem {
 
     // Max value from index 1 to 3, Range question
     static int MaxInRange(int[] array, int start, int end){
+        // edge cases
+        if ( array == null || array.length == 0 ) {
+            System.out.println("Array cannot be null or empty");
+            return 0;  // or return -1
+        }
+        if ( start < 0 || start > end || end >= array.length ) {
+            return 0;  // or return -1
+        }
+
+        //logic
         int max = array[start];
         for (int i = start; i <= end; i++) {
             if(array[i] > max){
