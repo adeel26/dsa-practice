3fda3aed6b7d22c6cc234bdbe652a5331861d3f4
Adeel Iqbal <iadeel26@gmail.com>
2025-10-29 11:17:40 -0700

    Questions: Strings: Goal Parser Interpretation updated

--- src/com/adeel/Questions/Strings/Q_Strings3.java
+++ src/com/adeel/Questions/Strings/Q_Strings3.java
@@ -4,6 +4,7 @@ public class Q_Strings3 {
     public static void main(String[] args) {
         String command = "G()()()()(al)";
         System.out.println(interpret(command));
+        System.out.println(interpretOptimized(command));
     }
     static String interpret(String command) {
         String output = "";
@@ -18,16 +19,40 @@ public class Q_Strings3 {
                 if(c[i+1] == ')')
                 {
                     output = output + 'o';
-                    i++; // skip the next ')'
+                    i++; // skip the next ')'  For Optimization
                 }
                 else if(c[i+1] == 'a')
                 {
                     output = output + "al";
-                    i = i+3; // skip 'a', 'l', and ')'
+                    i = i+3; // skip 'a', 'l', and ')' For Optimization
                 }
             }
         }
-
         return output;
+    }
+
+    static String interpretOptimized(String command){
+        StringBuilder output = new StringBuilder();
+        char[] c = command.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if(c[i] == 'G')
+            {
+                output.append('G');
+            }
+            if (c[i] == '(' && (i+1) < c.length)
+            {
+                if(c[i+1] == ')')
+                {
+                    output.append('o');
+                    i++; // skip the next ')'  For Optimization
+                }
+                else if(c[i+1] == 'a')
+                {
+                    output.append("al");
+                    i = i+3; // skip 'a', 'l', and ')' For Optimization
+                }
+            }
+        }
+        return output.toString();
     }
 }
