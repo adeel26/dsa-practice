b1da8952116e55cda92f6742e5f422df5cfe9f3a
Adeel Iqbal <iadeel26@gmail.com>
2025-10-29 11:06:54 -0700

    Questions: Strings: Goal Parser Interpretation

---
+++ src/com/adeel/Questions/Strings/Q_Strings3.java
@@ -0,0 +1,33 @@
+package com.adeel.Questions.Strings;
+// Goal Parser Interpretation
+public class Q_Strings3 {
+    public static void main(String[] args) {
+        String command = "G()()()()(al)";
+        System.out.println(interpret(command));
+    }
+    static String interpret(String command) {
+        String output = "";
+        char[] c = command.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if(c[i] == 'G')
+            {
+                output = output + 'G';
+            }
+            if (c[i] == '(' && (i+1) < c.length)
+            {
+                if(c[i+1] == ')')
+                {
+                    output = output + 'o';
+                    i++; // skip the next ')'
+                }
+                else if(c[i+1] == 'a')
+                {
+                    output = output + "al";
+                    i = i+3; // skip 'a', 'l', and ')'
+                }
+            }
+        }
+
+        return output;
+    }
+}
