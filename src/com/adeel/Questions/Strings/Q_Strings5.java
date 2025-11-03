e07f14c17692add8a60a24aab7c2cb83ceada445
Adeel Iqbal <iadeel26@gmail.com>
2025-11-03 05:23:27 -0800

    Questions: Strings: Sorting the Sentence updated

--- src/com/adeel/Questions/Strings/Q_Strings5.java
+++ src/com/adeel/Questions/Strings/Q_Strings5.java
@@ -1,19 +1,42 @@
 package com.adeel.Questions.Strings;
+
+import java.util.Arrays;
+
 // Sorting the Sentence
 public class Q_Strings5 {
     public static void main(String[] args) {
         String s = "is2 sentence4 This1 a3";
 
-        sortSentence(s);
+        System.out.println(sortSentence(s));
     }
 
     static String sortSentence(String s) {
-        String output = "";
-        System.out.println(s.substring(0, 1));
-        System.out.println(s.substring(0, 2));
-        System.out.println(s.substring(0, 3));
-        System.out.println(s.substring(2, 2));
+        // Split the sentence and get it in array
+        String[] parts = s.split(" ");
 
+        // Sort them according to numeric digit at end of every word
+        String[] ordered = new String[parts.length];
+        for (int i = 0; i < ordered.length ; i++) {
+            String part = parts[i]; // get each part at i position
+            char lastChar = part.charAt(part.length()-1);
+            int digit = Character.getNumericValue(lastChar); // Converting char to numeric. We can also do it by minus it with 0 and it works for 0-9
+
+            ordered[digit-1] = part; // digits are not index so we have to minus 1
+
+
+        }
+
+        String[] sentence = new String[ordered.length];
+        for (int i = 0; i < ordered.length; i++) {
+            // Removing last digit words in ordered array
+            String word = ordered[i];
+            sentence[i] = word.substring(0, word.length()-1);
+        }
+
+        System.out.println(Arrays.toString(ordered));
+        System.out.println(Arrays.toString(sentence));
+
+        String output = String.join(" ", sentence);
         return output;
     }
 }
