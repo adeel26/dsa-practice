6028f9489926c493256cf1471e174facedc0c2fb
Adeel Iqbal <iadeel26@gmail.com>
2025-11-09 23:21:36 -0800

    Question: Arrays: Find Numbers with Even Number of Digits updated

--- src/com/adeel/Questions/Array/Q_Array16.java
+++ src/com/adeel/Questions/Array/Q_Array16.java
@@ -2,6 +2,22 @@ package com.adeel.Questions.Array;
 // Find Numbers with Even Number of Digits
 public class Q_Array16 {
     public static void main(String[] args) {
-        
+        int[] nums = {437,315,322,431,686,264,442};
+        System.out.println(findNumbers(nums));
+    }
+    static int findNumbers(int[] nums) {
+        int evenCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int digitCount = 0;
+            while(nums[i] != 0){
+                nums[i] = nums[i] / 10;
+                digitCount++;
+            }
+            if(digitCount % 2 == 0){
+                evenCount++;
+            }
+        }
+        return evenCount;
     }
 }
