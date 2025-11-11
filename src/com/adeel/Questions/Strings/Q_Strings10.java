9d9150d30e85db89726b781e4a42d7d6b44b325b
Adeel Iqbal <iadeel26@gmail.com>
2025-11-10 22:06:29 -0800

    Question: Strings: Number of Strings That Appear as Substrings in Word

---
+++ src/com/adeel/Questions/Strings/Q_Strings10.java
@@ -0,0 +1,20 @@
+package com.adeel.Questions.Strings;
+// Number of Strings That Appear as Substrings in Word
+
+public class Q_Strings10 {
+    public static void main(String[] args) {
+        String[] patterns = {"a","abc","bc","d"};
+        String word = "abc";
+        System.out.println(numOfStrings(patterns, word));
+    }
+    static int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for (int i = 0; i < patterns.length ; i++) {
+            if(word.contains(patterns[i])){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
