9ea91d355e565cb81e8c804b07e43c32bb64e6c0
Adeel Iqbal <iadeel26@gmail.com>
2025-11-17 09:51:49 -0800

    OOP: Types Of Constructor

---
+++ src/com/adeel/oop/TypesOfConstructor.java
@@ -0,0 +1,41 @@
+package com.adeel.oop;
+
+public class TypesOfConstructor {
+    public static void main(String[] args) {
+        Students st1 = new Students();
+        System.out.println(st1.name);
+        System.out.println(st1.age);
+
+        Students st2 = new Students("Tony", 30);
+        System.out.println(st2.name);
+        System.out.println(st2.age);
+
+    }
+}
+
+class Students{
+    String name;
+    int age;
+
+    // Default Constructor
+    // there is no ambiguity btw local variables and instance variables, so we can skip "this" word.
+    Students(){
+        System.out.println("Default constructor called");
+        name = "Unknown";
+        age = 0;
+    }
+
+    // Parameterized Constructor
+    Students(String name, int age){
+        System.out.println("Parameterized constructor called");
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+/*
+Default constructor:
+Note: It is not necessary to write a constructor for a class because the Java compiler automatically creates a default constructor
+(a constructor with no arguments) if your class doesn’t have any.
+ */
