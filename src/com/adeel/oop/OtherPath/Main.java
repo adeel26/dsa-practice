24d41884478a198ce9ec8f09f53e150f72a2917e
Adeel Iqbal <iadeel26@gmail.com>
2025-11-20 02:30:45 -0800

    OOP: Main updated

--- src/com/adeel/oop/OtherPath/Main.java
+++ src/com/adeel/oop/OtherPath/Main.java
@@ -22,5 +22,31 @@ public class Main {
         System.out.println(car.isRunning);
         car.drive();
         car.brake();
+
+        // Student class
+        System.out.println("Student Class");
+
+        Student student1 = new Student("John", 30, 3.2);
+        Student student2 = new Student("Cena", 25, 2.5);
+        Student student3 = new Student("Batista", 26, 3.0);
+
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
+
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+
+        System.out.println(student3.name);
+        System.out.println(student3.age);
+        System.out.println(student3.gpa);
+        System.out.println(student3.isEnrolled);
+
+        student1.study();
+        student2.study();
+        student3.study();
     }
 }
