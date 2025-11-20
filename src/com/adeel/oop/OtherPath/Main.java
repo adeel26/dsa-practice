fe742145a9be5e04f095da19c820a9ea34365557
Adeel Iqbal <iadeel26@gmail.com>
2025-11-19 23:22:01 -0800

    OOP: Main updated

--- src/com/adeel/oop/OtherPath/Main.java
+++ src/com/adeel/oop/OtherPath/Main.java
@@ -2,9 +2,7 @@ package com.adeel.oop.OtherPath;
 
 public class Main {
     public static void main(String[] args) {
-        // Object = An entity that holds data (attributes)
-        //          and can perform actions (methods)
-        //          It is a reference data type
+    
 
         Car car = new Car();
         car.isRunning = true;
