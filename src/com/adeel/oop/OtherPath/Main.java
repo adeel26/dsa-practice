4edcad911d64af16d24a8488693a9be0b5e8329e
Adeel Iqbal <iadeel26@gmail.com>
2025-11-19 23:01:35 -0800

    OOP: Main

---
+++ src/com/adeel/oop/OtherPath/Main.java
@@ -0,0 +1,11 @@
+package com.adeel.oop.OtherPath;
+
+public class Main {
+    public static void main(String[] args) {
+        // Object = An entity that holds data (attributes)
+        //          and can perform actions (methods)
+        //          It is a reference data type
+
+        
+    }
+}
