8de0e7e09043bf7e8848414df13b3d73e14e9818
Adeel Iqbal <iadeel26@gmail.com>
2025-11-20 02:31:17 -0800

    OOP: Student Class updated

--- src/com/adeel/oop/OtherPath/Student.java
+++ src/com/adeel/oop/OtherPath/Student.java
@@ -1,9 +1,30 @@
 package com.adeel.oop.OtherPath;
 
 public class Student {
+    // Constructor = A special method to initialize objects
+    //               You can pass arguments to a constructor
+    //               and set up initial values
 
     String name;
     int age;
     double gpa;
     boolean isEnrolled;
+
+    // Constructor
+    Student() {
+
+    }
+
+    // Constructor
+    Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+        this.isEnrolled = true;
+    }
+
+    // Method
+    void study(){
+        System.out.println(this.name + " is studying");
+    }
 }
