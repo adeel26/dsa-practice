23c7d802f4e57e8e094478df715c1bf9a3fc7632
Adeel Iqbal <iadeel26@gmail.com>
2025-11-24 22:19:01 -0800

    OOP: CarMain updated

--- src/com/adeel/oop/OtherPath/CarMain.java
+++ src/com/adeel/oop/OtherPath/CarMain.java
@@ -23,30 +23,5 @@ public class CarMain {
         car.drive();
         car.brake();
 
-        // Student class
-        System.out.println("Student Class");
-
-        Student student1 = new Student("John", 30, 3.2);
-        Student student2 = new Student("Cena", 25, 2.5);
-        Student student3 = new Student("Batista", 26, 3.0);
-
-        System.out.println(student1.name);
-        System.out.println(student1.age);
-        System.out.println(student1.gpa);
-        System.out.println(student1.isEnrolled);
-
-        System.out.println(student2.name);
-        System.out.println(student2.age);
-        System.out.println(student2.gpa);
-        System.out.println(student2.isEnrolled);
-
-        System.out.println(student3.name);
-        System.out.println(student3.age);
-        System.out.println(student3.gpa);
-        System.out.println(student3.isEnrolled);
-
-        student1.study();
-        student2.study();
-        student3.study();
     }
 }
