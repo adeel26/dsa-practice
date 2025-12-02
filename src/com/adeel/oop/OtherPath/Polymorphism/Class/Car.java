16587632cf75fd669521ada86dc258bb8a3d5099
Adeel Iqbal <iadeel26@gmail.com>
2025-12-02 01:39:28 -0800

    OOP: Polymorphism via classes

---
+++ src/com/adeel/oop/OtherPath/Polymorphism/Class/Car.java
@@ -0,0 +1,9 @@
+package com.adeel.oop.OtherPath.Polymorphism.Class;
+
+public class Car extends Vehicle{
+
+    @Override
+    void go() {
+        System.out.println("You drive the car");
+    }
+}
