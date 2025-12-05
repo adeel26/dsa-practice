04b5076bb0afd56672ae3b5357252b5014d960a8
Adeel Iqbal <iadeel26@gmail.com>
2025-12-04 23:49:09 -0800

    Exception Handling

---
+++ src/com/adeel/Other_Path/ExceptionHandling/Main.java
@@ -0,0 +1,36 @@
+package com.adeel.Other_Path.ExceptionHandling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Exception = An event that interrupts the normal flow of a program
+        //             (Dividing by zero, file not found, mismatch input type)
+        //             Surround any dangerous code with a try{} block
+        //             try{}, catch{}, finally{}
+
+        Scanner input = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter a number: ");
+            int number = input.nextInt();
+            System.out.println(number);
+        }
+        catch(InputMismatchException e) {
+            System.out.println("That wasn't a number");
+        }
+        catch(ArithmeticException e) {
+            System.out.println("YOU CAN'T DIVIDE BY ZERO!");
+        }
+        catch (Exception e) {
+            // SAFETY NET
+            System.out.println("Something went wrong");
+        }
+        // Finally is optional. It always runs.
+        finally {
+            System.out.println("This always executes");
+            input.close();
+        }
+    }
+}
